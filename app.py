f5ff692681ac6f1c813fae893e7def910e6a1a3d
angaith <angaith@gmail.com>
2023-07-18 13:26:39 +0800

    implemented feature 1 functionality

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	return f"<p>Hello World!</p>"
+	features = []
+	feature1 = "Here's feature 1 functionality"
+	features.append(feature1)
+	return f"<p>{features}</p>"
